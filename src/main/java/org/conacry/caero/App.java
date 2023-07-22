d77d8d7c1e92a64700bb7d11992cbd4e2f652afe
Alexey Okruzhko <alex.okruzhko@gmail.com>
2023-07-22 18:53:35 +0600

    init commit

---
+++ src/main/java/org/conacry/caero/App.java
@@ -0,0 +1,11 @@
+package org.conacry.caero;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/org/conacry/caero/AppTest.java
@@ -0,0 +1,3 @@
+package org.conacry.caero;
+
+public class AppTest { }
