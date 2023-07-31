af82579a46bae76dd60cd08012db00b3a6fe44da
arabro <armankusa15@gmail.com>
2023-07-31 19:13:02 +0600

    feat (database connection): - DatabaseConfig - flyway file - IntegrationTest

---
+++ src/main/java/org/conacry/caero/adapter/repository/model/AirportDbModel.java
@@ -0,0 +1,18 @@
+package org.conacry.caero.adapter.repository.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class AirportDbModel {
+    private UUID airportID;
+    private String airportCode;
+    private String airportName;
+    private String city;
+    private String latitude;
+    private String longitude;
+    private String timezone;
+}
